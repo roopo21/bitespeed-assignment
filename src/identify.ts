5217982160057aefcbe468ab47b49fcfb8d4d91d
Abhiroop Mareedu <roopo@Abhiroops-MacBook-Pro.local>
2025-06-30 23:48:58 +0530

    working code

--- src/identify.ts
+++ src/identify.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
-import { error } from 'console';
+import { PrismaClient, Contact } from './generated/prisma';
+import {IdentifyResponse } from './types';
 
 
 export default async function identify(req: Request, res: Response, prisma: PrismaClient) {
@@ -22,9 +22,8 @@ export default async function identify(req: Request, res: Response, prisma: Pris
         orderBy: { createdAt: 'asc' }
     });
 
-    const primaryContact = contacts.find((contact: { linkPrecedence: string; }) => contact.linkPrecedence === 'primary') || contacts[0]; // add types.
+    const primaryContact = contacts.find((contact: Contact) => contact.linkPrecedence === 'primary') || contacts[0];
     if (!primaryContact) {
-        // no primary -> create new primary contact
         const newContact = await prisma.contact.create({
             data: {
                 email, phoneNumber, linkPrecedence: 'primary',
@@ -41,7 +40,45 @@ export default async function identify(req: Request, res: Response, prisma: Pris
     }
 
     const relatedContacts = await prisma.contact.findMany({
-        
+        where: {
+            OR: [
+                { linkedId: primaryContact.id },
+                { id: primaryContact.id },
+            ]
+        },
+        orderBy: { createdAt: 'asc' }
+    });
 
-    })
+    const alreadyExists = relatedContacts.some(
+        (c: Contact) => c.email === email && c.phoneNumber === phoneNumber
+    );
+    if (!alreadyExists && (email || phoneNumber)) {
+        const newSecondary = await prisma.contact.create({
+            data: {
+                email,
+                phoneNumber,
+                linkedId: primaryContact.id,
+                linkPrecedence: 'secondary'
+            }
+        });
+
+        relatedContacts.push(newSecondary);
+    }
+    const emails = Array.from(
+        new Set(relatedContacts.map((c: Contact) => c.email).filter(Boolean))
+    );
+    const phones = Array.from(new Set(relatedContacts.map((c: Contact) => c.phoneNumber).filter(Boolean)));
+
+    const secondaryIds = relatedContacts
+        .filter((c: Contact) => c.linkPrecedence === 'secondary')
+        .map((c: Contact) => c.id);
+
+    res.status(200).json({
+        contact: {
+            primaryContatctId: primaryContact.id,
+            emails: [primaryContact.email, ...emails.filter(e => e !== primaryContact.email)],
+            phoneNumbers: [primaryContact.phoneNumber, ...phones.filter(p => p !== primaryContact.phoneNumber)],
+            secondaryContactIds: secondaryIds
+        }
+    });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from './generated/prisma';
 import dotenv from 'dotenv';
 import identify from './identify';
 // import ident
@@ -13,3 +13,7 @@ const PORT = process.env.port || 3000;
 app.use(express.json());
 
 app.post('/identify', (req,res) => identify(req, res, primsa));
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
